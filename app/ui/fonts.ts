0e1fbfa9e19672a0d8ca92b7e83f7065d4dcc1cb
abeer <marksmanabr@gmail.com>
2024-06-19 14:16:05 +0600

    added elements to dashboard

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import {Inter} from 'next/font/google';
+import {Inter, Lato} from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 export const inter =Inter({subsets:['latin']});
 export const lusitana =Lusitana({subsets:['latin'],weight:['400']});
+export const lato = Lato({subsets:['latin'],weight:['400']});
